45e8c7c1296cf01b5c26fefb8d7a5cc4306d91a2
Milka Cutipa <adac-mk@hotmail.com>
2026-02-24 13:01:15 -0400

    feat: add exercise 2.2

--- exercise_2.js
+++ exercise_2.js
@@ -45,3 +45,42 @@ console.log(`La frase tiene ${countVowels(phrase4)} vocales`)
 //prueba 5
 const phrase5 = "ENTRE PALABRAS QUE VAN Y VIENEN, SE ESCONDE UNA HISTORIA SILENCIOSA, QUE EL TIEMPO LEE CON CALMA LENTA"; //39
 console.log(`La frase tiene ${countVowels(phrase5)} vocales`)
+
+
+/*
+2.2 Un cliente necesita una función que duplique cada número en un array (multiplicar por 2),
+ pero nos dice que el array original también se está modificando y no quiere eso.
+1. Identifica el bug - ¿Por qué se modifica el array original?
+    Al ejecutar la función duplicateNumbers, se envía una referencia al array original.
+    Dentro de la función se modifican sus valores mediante la multiplicación, por lo que los 
+    cambios afectan tanto al array original como al resultado retornado..
+2. Explica el concepto - ¿Qué significa "pasar por referencia"?
+    El concepto se refiere que cuando tratas de copiar un valor como objetos o array, no se copia
+    el valor como tal, si no que pasas una referencia, apuntado al objeto inicial, y al editar dicha
+    copia, se modifica tambien el valor original
+3. Arregla el código - Haz que NO modifique el original
+    Se soluciona usando la funcion duplicateNumbers, el cual crea un nuevo array copiando al origial
+    y no referenciandolo 
+
+4. Bonus: Reescríbelo usando .map()
+    Algo que me di cuenta es que se puede referencial directamente al array original, y este no
+    sera modifcado, por lo que map al ejecutarse crea un nuevo array evitando modificar la original
+function
+*/
+
+function duplicateNumbers(numbers) {
+     for (let i = 0; i < numbers.length; i++) { 
+        numbers[i] = numbers[i] * 2; 
+    }
+    return numbers; 
+} 
+
+const original = [1, 2, 3, 4, 5]; 
+const duplicated_array = original
+const duplicated = duplicateNumbers(duplicated_array); 
+console.log("Original:", original); 
+console.log("Duplicated:", duplicated);
+
+//const duplicated_map = original
+const duplicatedmap = original.map(num=>num*2)
+console.log("Usnado la funcion map: ",duplicatedmap)
