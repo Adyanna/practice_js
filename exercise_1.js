1d1f82227fda69e7245156d593978c3b97d4898e
Milka Cutipa <adac-mk@hotmail.com>
2026-02-24 11:07:35 -0400

    feat: add first exercise

---
+++ exercise_1.js
@@ -0,0 +1,12 @@
+//PRESENTACION
+
+let perfil = {
+    first_name:"Adriana Milka",
+    last_name:"Cutipa Quispe",
+    bootcamp_topics:['Programacion 101','Ingeniera de Software 101','JavaScript','git, GitHub y SourceTree','Linux'],
+    previous_word_exp: true,
+    social_links:[{name:'Linkedin',link:'www.linkedin.com/in/adriana-cutipa-275b72198'},
+        {name:'github',link:'https://github.com/Adyanna'},
+        {name:'gitlab',link:'https://gitlab.com/Milka_Cqa'},
+    ]
+}
